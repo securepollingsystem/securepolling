096e2d2cce63ad0b2f7b11db71c3584b1c8f1205
tlevine <tlevine>
2018-02-03 23:39:41 +0000

    registrar

---
+++ securepolling/registrar.py
@@ -0,0 +1,32 @@
+def import_calendar():
+    pass
+
+def confirm_eligibility(identity):
+    '''
+    Confirm that a particular identity is eligible to vote.
+    If the identity has not been submitted before, queue the identity for
+    confirmation. If it has been submitted but not reviewed, report the date of
+    submission. If it has been reviewed, report the result and, if confirmed,
+    subkeys for blinded key submission.
+
+    :param identity: A unique string with information that the registrar will
+    use to verify user identity in person.
+    '''
+
+def appointment_schedule(identity, blinded_key, appointment_selection):
+    '''
+    Check that the identity's eligibility has been confirmed and that the
+    selected appointment is available. If both are true, tell the voter
+    and put it on the calendar. If either is not, report an error.
+    '''
+
+def appointment_availabilities():
+    '''
+    List available appointment slots.
+    '''
+
+def sign_key(identity, date, registrar_key):
+    '''
+    Record the identity and date, and sign the voter's blinded key (stored
+    already in the database) with the registar key.
+    '''
