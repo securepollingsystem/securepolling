c500d899b06abab50e6a58c60140830d212d6a11
tlevine <tlevine>
2018-02-21 17:17:41 +0000

    example

--- doc/securepolling.sh
+++ doc/securepolling.sh
@@ -1,10 +1,10 @@
-alias sps 'python3 -m securepolling'
+sps='python3 -m securepolling'
 rm -f ~/.config/securepolling.json
-sps registrar add_slot /tmp/registrar.sqlite 2018-0{2,3}-01T00:00:00 -length 20
-sps poller create /tmp/registrar.sqlite tom
-sps poller keygen
-sps poller calendar
-sps poller schedule_appointment 2018-02-18T16:34:00
-sps registrar confirm_eligibility /tmp/registrar.sqlite tom
-sps poller confirm_appointment
-sps registrar confirm_eligibility /tmp/registrar.sqlite tom
+$sps registrar add_slot /tmp/registrar.sqlite 2018-02-1{7,9}T00:00:00 -length 20
+$sps poller create /tmp/registrar.sqlite tom
+$sps poller keygen
+$sps poller calendar
+$sps poller schedule_appointment 2018-02-17T16:34:00
+$sps registrar confirm_eligibility /tmp/registrar.sqlite tom
+$sps poller confirm_appointment
+$sps registrar confirm_eligibility /tmp/registrar.sqlite tom
