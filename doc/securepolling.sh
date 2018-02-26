5fb9365b01998b3479433311fa3e59b9a359eeb2
tlevine <tlevine>
2018-02-26 20:25:37 +0000

    charts

--- doc/securepolling.sh
+++ doc/securepolling.sh
@@ -3,9 +3,9 @@ sps='python3 -m securepolling'
 rm -f ~/.config/securepolling.json /tmp/registrar.sqlite
 
 $sps registrar add_slot /tmp/registrar.sqlite 2018-02-1{7,9}T00:00:00 -length 20
-$sps poller create /tmp/registrar.sqlite tom
-$sps poller calendar
-$sps poller schedule_appointment 2018-02-17T16:34:00
-$sps registrar confirm_eligibility /tmp/registrar.sqlite tom
-$sps poller confirm_appointment
-$sps registrar confirm_eligibility /tmp/registrar.sqlite tom
+$sps pollee create /tmp/registrar.sqlite tom
+$sps pollee calendar
+$sps pollee schedule_appointment 2018-02-17T16:34:00
+$sps registrar confirm_eligibility /tmp/registrar.sqlite tom yes
+$sps pollee confirm_appointment
+$sps registrar verify_identity tom
